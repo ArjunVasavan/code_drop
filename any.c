f42233d4fba189d071dd1fb7ba25625081f013b7
SanjaiScript <sanjaimkumar1223@gmail.com>
2026-01-29 09:10:15 +0000

    Testing

--- any.c
+++ any.c
@@ -1,27 +1,29 @@
 #include "sll.h"
 
-int sl_insert_after(Slist **head, data_t g_data, data_t ndata)
+int sl_insert_before(Slist **head, data_t g_data, data_t ndata)
 {
     if(*head == NULL){
         return LIST_EMPTY;
     }
     Slist *temp = *head;
-
-    while(temp != NULL){
-        if(temp->data != g_data){
-            temp = temp->link;
-        }
-        else{
-            Slist *new = malloc(sizeof(Slist));
-        
-            if(!new){
-            return FAILURE;
-            }
-            new->data = ndata;
+    Slist *new = malloc(sizeof(Slist));
+    if(!new){
+        return FAILURE;
+    }
+    new->data = ndata;
+    if((*head)->data == g_data){
+        new->link = *head;
+        *head = new;
+        return SUCCESS;
+    }
+    while(temp->link != NULL){
+        if(temp->link->data == g_data){
             new->link = temp->link;
             temp->link = new;
             return SUCCESS;
         }
+        temp = temp->link;
     }
+    free(new);
     return DATA_NOT_FOUND;
 }
