f87c3c1e3d9ac85ddc5cce54964b49adb56bf023
Arjun Vasavan <vasavanarjun@gmail.com>
2026-02-11 12:03:07 +0530

    w

---
+++ assignment_12/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define SUCCESS 0
+#define FAILURE -1
+
+typedef int data_t;
+typedef struct node
+{
+	data_t data;
+	struct node *link;
+}Stack_t;
+
+
+int Push(Stack_t **top, data_t data);
+int Pop(Stack_t **top);
+int Peek(Stack_t **top);
+int Peep(Stack_t *top);
+#endif
