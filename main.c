156fcc84a829230850809b4a23e7e5aa370de701
Arjun Vasavan <vasavanarjun@gmail.com>
2026-03-30 11:51:58 +0530

    send: main.c [30-03-2026 11:51]

--- main.c
+++ main.c
@@ -1,149 +1,202 @@
-#include <xc.h>
-
-/* NOTE: Logic
- * mode = 0 => run mode
- * mode = 1 => edit mode
- * Run mode:
- *   - Clock ticks via TMR0 ISR (count -> sec -> min -> hr)
- *   - Colon (DP) blinks every ~500ms using count threshold
- *   - Switches 1/2/3 do nothing in run mode
- *   - Switch 4 toggles into edit mode
- * Edit mode:
- *   - Clock does NOT tick (sec/count frozen)
- *   - Switch 3 toggles which field is selected (hr / min)
- *   - Switch 1 increments selected field
- *   - Switch 2 decrements selected field
- *   - Selected field blinks (00XX -> 0000 -> 00XX)
- *   - Switch 4 exits back to run mode
+/*
+ * File:   main.c
+ * Author: DELL
+ *
+ * Created on 26 March, 2026, 12:39 PM
  */
 
-volatile unsigned long int sec   = 0;
-volatile unsigned long int count = 0;
 
-char digit[] = {0xE7, 0x21, 0xCB, 0x6B, 0x2D, 0x6E, 0xEE, 0x23, 0xEF, 0x6F};
+#include <xc.h>
+#define EDGE 1
+signed char hr = 0;
+signed char min = 0;
+char sec = 0;
+char mode = 0;
+int count = 0;
+char flag = 1;
+char field_flag = 0;
+char digit[10] = {0xE7,0x21,0xCB,0x6B,0x2D,0X6E,0xEE,0x23,0xEF,0x6F}; // 7-seg codes
+char ssd[4];     // 4-digit display buffer
 
-void __interrupt() isr(void) {
-    if (INTCONbits.TMR0IF) {
-        INTCONbits.TMR0IF = 0;
-        TMR0 += 8;        
-        if (++count >= 20000) {
-            count = 0;   
+void __interrupt() isr()
+{
+    if(TMR0IF)
+    {
+        TMR0 = 8 + TMR0;
+        
+        if(count++ == 20000)
+        {
             sec++;
+            count = 0;
         }
+        TMR0IF = 0;
     }
 }
 
-char read_digital_keypad(void) {
-    return (PORTC & 0x0F); 
-}
-
-void display(char data[]) {
-    for (unsigned int d = 0; d < 4; d++) {
-        PORTD = data[d];
-        PORTA = (PORTA & 0xF0) | (1u << d);
-        for (unsigned int delay = 1000; delay--;);
+void display(char *data)
+{
+    for( int i = 0 ; i < 4 ; i++ )
+    {
+        PORTD = data[i];   // Send segment data
+        PORTA = (unsigned char)((PORTA & 0XF0) | (1 << i)); // Select digit
+        for( int delay_1 = 500; delay_1--; ); // Multiplexing delay
     }
 }
 
-static char prev_key = 0;
-char key_pressed(char key, char bit) {
-    return (key & bit) && !(prev_key & bit);
+char read_digital_keypad(char key) 
+{
+    if (key == EDGE)
+    {
+        if ((PORTC & 0x0F) != 0x0F && flag)
+        {
+            flag = 0;
+            return PORTC & 0x0F;
+        }
+        else if((PORTC & 0x0F) == 0x0F)
+        {
+            flag = 1;
+        }
+        
+    }
+    return 0x0F; 
 }
 
-int main(void) {
-    int hr   = 0;
-    int min  = 0;
-    int mode = 0;
-    int edit_field = 0;
-
-    char ssd[4];
-    char key;
-
-    TRISD  = 0x00; // TRISD -> Seven segment
-    TRISA &= 0xF0; // Controller pin
-    TRISC |= 0x0F; // TRISC -> that pull up keyboard    
-
-    T0CON             = 0xC4;  
-    TMR0              = 8;
-    INTCONbits.TMR0IE = 1;
-    INTCONbits.GIE    = 1;
-
-    while (1) {
-
-        key = read_digital_keypad();
-
-        if (key_pressed(key, 0x0E)) {
+void main(void) {
+    
+    TRISC = TRISC | 0X0F;
+    TRISD = 0x00;
+    TRISA = TRISA & 0xF0;
+    PORTA = PORTA & 0XF0;
+    
+    
+    T08BIT = 1;
+    T0CS = 0;
+    PSA = 1;
+    GIE = 1;
+    PEIE = 1;
+    TMR0IE = 1;
+    TMR0IF = 0;
+    TMR0 = 6;
+    TMR0ON = 1;
+    ADCON1 = 0x0F;
+    while(1)
+    {
+        char key = read_digital_keypad(EDGE);
+        
+        if(key == 0x07)
+        {
             mode = !mode;
-            if (mode == 1) {
-                INTCONbits.TMR0IE = 0;
-                edit_field = 0;
-            } else {
-                sec   = 0;
-                count = 0;
-                INTCONbits.TMR0IE = 1;
-            }
+            sec = 0;
         }
-
-        if (mode == 0) {
-            if (sec >= 60) {
+        else if(key == 0x0B)
+        {
+            field_flag = !field_flag;
+        }
+        
+        
+        if(mode == 0)
+        {
+            if(sec == 60)
+            {
                 sec = 0;
                 min++;
-                if (min >= 60) {
-                    min = 0;
+                if(min == 60)
+                {
                     hr++;
-                }
-                if (hr >= 24) {
-                    hr = 0;
-                }
-            }
-
-            ssd[0] = digit[hr / 10];
-            ssd[1] = (count < 10000)
-                ? (0x10 | digit[hr % 10])
-                :          digit[hr % 10];
-            ssd[2] = digit[min / 10];
-            ssd[3] = digit[min % 10];
-
-        } else {
-            if (key_pressed(key, 0x0B)) {
-                edit_field = !edit_field;
-            }
-
-            if (key_pressed(key, 0x07)) {
-                if (edit_field == 0) {
-                    hr  = (hr  + 1) % 24;
-                } else {
-                    min = (min + 1) % 60;
+                    min = 0;
+                    if(hr == 24)
+                    {
+                        hr = 0;
+                    }
                 }
             }
-
-            if (key_pressed(key, 0x0D)) {
-                if (edit_field == 0) {
-                    hr  = (hr  + 23) % 24;
-                } else {
-                    min = (min + 59) % 60;
-                }
-            }
-
-            static unsigned long blink_count = 0;
-            blink_count++;
-            char blink_on = (blink_count < 30000);
-            if (blink_count >= 60000) blink_count = 0;
-
-            if (edit_field == 0) {
-                ssd[0] = blink_on ? digit[hr / 10]  : 0x00;
-                ssd[1] = blink_on ? digit[hr % 10]  : 0x00;
-                ssd[2] = digit[min / 10];
-                ssd[3] = digit[min % 10];
-            } else {
-                ssd[0] = digit[hr / 10];
-                ssd[1] = digit[hr % 10];
-                ssd[2] = blink_on ? digit[min / 10] : 0x00;
-                ssd[3] = blink_on ? digit[min % 10] : 0x00;
-            }
+            ssd[0] = digit[hr/10];
+            ssd[2] = digit[min/10];
+            ssd[3] = digit[min%10];
+            
+            if(count < 10000)
+                ssd[1] = 0x10 | digit[hr%10];
+            else
+                ssd[1] = digit[hr%10];
+            
+            display(ssd);    
         }
-
-        prev_key = key; 
-        display(ssd);
+        else if(mode == 1)
+        {
+            if(key == 0x0E)
+            {
+                if(field_flag == 0)
+                {
+                    min++;
+                    if(min == 60)
+                    {
+                        hr++;
+                        min = 0;
+                        if(hr == 24)
+                        {
+                            hr = 0;
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    hr++;
+                    if((hr == 24))
+                    {
+                        hr = 0;
+                    }
+                }    
+            }
+            else if(key == 0x0D)
+            {
+                if(field_flag == 0)
+                {
+                    min--;
+                    if(min == -1)
+                    {
+                        hr--;
+                        min = 59;
+                        if(hr == -1)
+                        {
+                            hr = 23;
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    hr--;
+                    if(hr == -1 )
+                    {
+                        hr = 23;
+                    }
+                }
+            }
+            
+            if(count < 10000)
+            {   
+                ssd[2] = digit[min/10];
+                ssd[3] = digit[min%10];
+                ssd[0] = digit[hr/10];
+                ssd[1] = digit[hr%10];      
+            }
+            else
+            {
+                if(field_flag == 0)
+                {
+                    ssd[2] = 0;
+                    ssd[3] = 0;
+                }
+                else
+                {
+                    ssd[0] = 0;
+                    ssd[1] = 0;  
+                }    
+            }
+            display(ssd);        
+        }
     }
+
+    return;
 }
